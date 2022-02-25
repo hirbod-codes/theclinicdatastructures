acc7059528da7590757d9a0e0fec87bbe45fe437
Hirbod <hirbod.khatami.words@gmail.com>
2022-02-25 03:15:07 -0800

    Implement protected validateVisitsType parent method in DSRegularOrder

--- Src/DataStructures/Order/Regular/DSRegularOrder.php
+++ Src/DataStructures/Order/Regular/DSRegularOrder.php
@@ -3,7 +3,20 @@
 namespace TheClinicDataStructures\DataStructures\Order\Regular;
 
 use TheClinicDataStructures\DataStructures\Order\DSOrder;
+use TheClinicDataStructures\DataStructures\Visit\DSVisits;
+use TheClinicDataStructures\DataStructures\Visit\Regular\DSRegularVisits;
+use TheClinicDataStructures\Exceptions\DataStructures\Order\InvalidValueTypeException;
 
 class DSRegularOrder extends DSOrder
 {
+    protected function validateVisitsType(DSVisits|null $visits): void
+    {
+        if ($visits === null) {
+            return;
+        }
+
+        if (!($visits instanceof DSRegularVisits)) {
+            throw new InvalidValueTypeException("This data structure only accepts the type: " . DSRegularVisits::class . " as it's associated visits.", 500);
+        }
+    }
 }
