b91eca3603b0d3479cbbc336220755923e447ff0
Hirbod <hirbod.khatami.words@gmail.com>
2022-02-26 20:45:10 -0800

    Add a DSUser parameter in IUserRule interface methods

--- Src/DataStructures/User/IUserRule.php
+++ Src/DataStructures/User/IUserRule.php
@@ -4,8 +4,16 @@ namespace TheClinicDataStructures\DataStructures\User;
 
 interface IUserRule
 {
-    public function getName(): string;
-    public function privilegeExists(string $privilege): bool;
-    public function getPrivileges(): array;
-    public function getPrivilegeValue(string $privilege): mixed;
+    public function getRuleName(): string;
+    
+    public function privilegeExists(DSUser $user, string $privilege): bool;
+
+    /**
+     * @return array [ "privilege_name" => privilege_value, ... ]
+     */
+    public function getPrivileges(DSUser $user): array;
+    
+    public function getPrivilegeValue(DSUser $user, string $privilege): mixed;
+
+    public function setPrivilege(DSUser $user, string $privilege): void;
 }
