dd24b752b8aa1c50ee26b124c06c246098a7e330
Hirbod <hirbod.khatami.words@gmail.com>
2022-04-02 21:42:08 -0700

    Change DSOrders to a non abstract class

--- Src/DataStructures/Order/DSOrders.php
+++ Src/DataStructures/Order/DSOrders.php
@@ -9,7 +9,7 @@ use TheClinicDataStructures\Exceptions\DataStructures\Order\InvalidOffsetTypeExc
 use TheClinicDataStructures\Exceptions\DataStructures\Order\InvalidUserException;
 use TheClinicDataStructures\Exceptions\DataStructures\Order\InvalidValueTypeException;
 
-abstract class DSOrders implements \ArrayAccess, \Iterator, \Countable
+class DSOrders implements \ArrayAccess, \Iterator, \Countable
 {
     use TraitKeyPositioner;
 
