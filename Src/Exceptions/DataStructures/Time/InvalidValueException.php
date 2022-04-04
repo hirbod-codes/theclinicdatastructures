da4be459e0ed186d0f24f1dd94710a2d7912c47f
Hirbod <hirbod.khatami.words@gmail.com>
2022-04-03 23:52:32 -0700

    Create InvalidValueException exception

---
+++ Src/Exceptions/DataStructures/Time/InvalidValueException.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace TheClinicDataStructures\Exceptions\DataStructures\Time;
+
+use TheClinicDataStructures\Exceptions\DataStructures\Time\TimeExceptions;
+
+class InvalidValueException extends TimeExceptions
+{
+}
