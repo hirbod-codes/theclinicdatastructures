c9e4b7969314da52ad01c121bb77d5163099ae28
Hirbod <hirbod.khatami.words@gmail.com>
2022-04-28 23:36:02 -0700

    Add dsPatients property to DSOperator

--- Src/DataStructures/User/DSOperator.php
+++ Src/DataStructures/User/DSOperator.php
@@ -2,8 +2,62 @@
 
 namespace TheClinicDataStructures\DataStructures\User;
 
+use TheClinicDataStructures\DataStructures\Order\DSOrders;
+
 class DSOperator extends DSUser
 {
+    public DSPatients|null $dsPatients;
+
+    /**
+     * @param ICheckAuthentication $iCheckAuthentication
+     * @param integer $id
+     * @param string $firstname
+     * @param string $lastname
+     * @param string $username
+     * @param string $gender
+     * @param string $phonenumber
+     * @param \DateTime $phonenumberVerifiedAt
+     * @param \DateTime $createdAt
+     * @param \DateTime $updatedAt
+     * @param DSPatients|null|null $dsPatients
+     * @param string|null|null $email
+     * @param \DateTime|null|null $emailVerifiedAt
+     * @param DSOrders|null|null $orders
+     */
+    public function __construct(
+        ICheckAuthentication $iCheckAuthentication,
+        int $id,
+        string $firstname,
+        string $lastname,
+        string $username,
+        string $gender,
+        string $phonenumber,
+        \DateTime $phonenumberVerifiedAt,
+        \DateTime $createdAt,
+        \DateTime $updatedAt,
+        DSPatients|null $dsPatients = null,
+        string|null $email = null,
+        \DateTime|null $emailVerifiedAt = null,
+        DSOrders|null $orders = null,
+    ) {
+        parent::__construct(
+            $iCheckAuthentication,
+            $id,
+            $firstname,
+            $lastname,
+            $username,
+            $gender,
+            $phonenumber,
+            $phonenumberVerifiedAt,
+            $createdAt,
+            $updatedAt,
+            $email,
+            $emailVerifiedAt,
+            $orders
+        );
+
+        $this->dsPatients = $dsPatients;
+    }
     public function getRuleName(): string
     {
         return 'operator';
