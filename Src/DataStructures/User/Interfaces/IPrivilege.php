c66a13dcfac5e93c30d83f90add7c664a8a4c178
Hirbod <hirbod.khatami.words@gmail.com>
2022-05-17 08:01:35 -0700

    Add getUserPrivileges method to IPrivilege interface

--- Src/DataStructures/User/Interfaces/IPrivilege.php
+++ Src/DataStructures/User/Interfaces/IPrivilege.php
@@ -7,4 +7,9 @@ use TheClinicDataStructures\DataStructures\User\DSUser;
 interface IPrivilege
 {
     public function setPrivilege(DSUser $user, string $privilege, mixed $value): void;
+
+    /**
+     * @return array<string, string> ['privilege' => value, ...]
+     */
+    public function getUserPrivileges(): array;
 }
