269ba5e8bc83d1e234729b278498bbba5e29499d
Hirbod <hirbod.khatami.words@gmail.com>
2022-05-18 06:58:21 -0700

    Update IPrivilege interface

--- Src/DataStructures/User/Interfaces/IPrivilege.php
+++ Src/DataStructures/User/Interfaces/IPrivilege.php
@@ -6,10 +6,10 @@ use TheClinicDataStructures\DataStructures\User\DSUser;
 
 interface IPrivilege
 {
-    public function setPrivilege(DSUser $user, string $privilege, mixed $value): void;
+    public function setPrivilege(DSUser $dsUser, string $privilege, mixed $value): void;
 
     /**
      * @return array<string, string> ['privilege' => value, ...]
      */
-    public function getUserPrivileges(): array;
+    public function getUserPrivileges(DSUser $dsUser): array;
 }
